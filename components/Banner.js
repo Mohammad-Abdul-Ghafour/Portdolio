626d048519ba509bb2c004a3fde4374ddc50e1bf
Mohammad Abdul Ghafour <mohammad1478971539@gmail.com>
2022-01-13 22:02:06 +0200

    text color pallet

--- components/Banner.js
+++ components/Banner.js
@@ -10,9 +10,11 @@ const Banner = () => {
     return (
         <div className='flex bg-slate-900 w-full' id='headerNav'>
             <div className='w-1/2 mt-20'>
-                <h1>Mohammad Abdul Ghafour</h1>
-                <h3>Computers Eng / Web Dev</h3>
-                <div className='w-1/2 h-full place-items-end'>
+                <div className='text-right'>
+                    <h1 className='text-white'>Mohammad Abdul Ghafour</h1>
+                    <h3 className='text-rose-600'>Computers Eng / Web Dev</h3>
+                </div>
+                <div className='w-1/2 h-full'>
                     <Image src={me} alt="Mohammad Abdul Ghafour" />
                 </div>
 
